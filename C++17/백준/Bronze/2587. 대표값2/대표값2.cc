0e87a460da2c4d24bfe8f9e798a20f7835ae9e7c
Catus-Kim <72134346+Catus-Kim@users.noreply.github.com>
2026-03-08 19:55:19 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2587. 대표값2/대표값2.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    /* code */ 
+    int arr[5];
+    int sum = 0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    sort(arr, arr + 5);
+
+    cout << sum / 5 << '\n';
+    cout << arr[2];
+
+    return 0;
+}
